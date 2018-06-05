dfe0a3e71fcac25b3e307dad6f07f4c24e262c88
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-05 10:07:03 +0200

    film model y mysql

---
+++ filmModel2.js
@@ -0,0 +1,20 @@
+const Conection = require('../helpers/mysqlconnection');
+
+
+
+let films = {};
+
+films.fetchAll = (cb) => {
+    if(!Conn) return cb("No se ha creado la conexion");
+//buena practica. Comprueba si hay conexion, sino la hay devuelve un mensaje.//
+    const SQL = "SELECT * FROM sakila.film LIMIT 10;";
+// Envia una orden a la base de datos, aunque es un string aqui,// 
+// tiene que terminar en ; porque es una orden en la base de datos.//      
+    Conn.query(SQL, (error, rows) => {
+        if (error) return cb(error);
+        else return cb(rows);
+// cb es un callback llama a la funcion.//        
+    });  
+};
+
+module.exports = films;
---
+++ mysqlconnection.js
@@ -0,0 +1,10 @@
+const MYSQL = require('mysql');
+const CONN = MYSQL.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '44865710',
+    database: 'sakila' 
+});
+
+module.exports = CONN;
+
